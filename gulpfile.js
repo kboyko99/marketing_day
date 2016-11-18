1f2db281a8cbe8343f8e766d74ab0c4cbc0aaced
degtyar <firstyuriy@gmail.com>
2016-11-18 20:35:08 +0200

    responsive tickets

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
   return gulp.src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());;
 });
 
 gulp.task('sass:watch', function () {
+  livereload.listen();
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
